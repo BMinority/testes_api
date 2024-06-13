a1916649634ca0e0314e1053af17d9bdebc0e734
bruno <brunocoelho66@gmail.com>
2024-06-13 19:17:49 -0300

    criei o arquivo dindin.sql

---
+++ API_DESAFIO_CUBOS/src/dindin.sql
@@ -0,0 +1,44 @@
+-- Criação da tabela usuarios
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
+
+-- Criação da tabela categorias
+CREATE TABLE categorias (
+    id SERIAL PRIMARY KEY,
+    descricao VARCHAR(255) NOT NULL
+);
+
+-- Criação da tabela transacoes
+CREATE TABLE transacoes (
+    id SERIAL PRIMARY KEY,
+    descricao VARCHAR(255) NOT NULL,
+    valor NUMERIC(10, 2) NOT NULL,
+    data DATE NOT NULL,
+    categoria_id INTEGER REFERENCES categorias(id),
+    usuario_id INTEGER REFERENCES usuarios(id),
+    tipo VARCHAR(50) NOT NULL
+);
+
+-- Inserção das categorias iniciais
+INSERT INTO categorias (descricao) VALUES
+('Alimentação'),
+('Assinaturas e Serviços'),
+('Casa'),
+('Mercado'),
+('Cuidados Pessoais'),
+('Educação'),
+('Família'),
+('Lazer'),
+('Pets'),
+('Presentes'),
+('Roupas'),
+('Saúde'),
+('Transporte'),
+('Salário'),
+('Vendas'),
+('Outras receitas'),
+('Outras despesas');
